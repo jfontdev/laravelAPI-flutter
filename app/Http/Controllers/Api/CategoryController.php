71f5d8aefc21229ba65ef938e90e851a8c52e154
jfontvit <jordifv90@gmail.com>
2023-02-20 15:56:44 +0100

    Add CategoryResource wrapping to our get method

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -12,9 +13,9 @@ class CategoryController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(): \Illuminate\Database\Eloquent\Collection
+    public function index(): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
     {
-        return Category::all();
+        return CategoryResource::collection(Category::select('id','name')->get());
     }
 
     /**
