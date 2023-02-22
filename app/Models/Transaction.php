64fa06df14de86a4c4a8d76734d383076c1567fd
jfontvit <jordifv90@gmail.com>
2023-02-22 10:36:05 +0100

    Add model and migration of transactions table

---
+++ app/Models/Transaction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['category_id', 'transaction_date', 'amount', 'description'];
+
+    protected $casts = [
+        'transaction_date' => 'date'
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+}
